f77e275b7075def96c48664b37ecd357f4b085c8
Matthias Wagner <matthias.wagner@online.de>
2012-04-19 16:00:41 +0300

    debug

--- app/controllers/spree/static_content_controller.rb
+++ app/controllers/spree/static_content_controller.rb
@@ -6,6 +6,7 @@ class Spree::StaticContentController < Spree::BaseController
   layout :determine_layout
   
   def show
+    logger.info params[:path]
     path = case params[:path]
     when Array
       '/' + params[:path].join("/")
